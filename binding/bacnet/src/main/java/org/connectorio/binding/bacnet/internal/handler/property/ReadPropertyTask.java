242e7c5e8466bffdb022222675a031279a58f37b
Łukasz Dywicki <luke@code-house.org>
2020-02-04 22:06:36 +0100

    Fix mapping for missing bacnet value types.

--- binding/bacnet/src/main/java/org/connectorio/binding/bacnet/internal/handler/property/ReadPropertyTask.java
+++ binding/bacnet/src/main/java/org/connectorio/binding/bacnet/internal/handler/property/ReadPropertyTask.java
@@ -2,14 +2,21 @@ package org.connectorio.binding.bacnet.internal.handler.property;
 
 import com.serotonin.bacnet4j.type.Encodable;
 import com.serotonin.bacnet4j.type.enumerated.BinaryPV;
+import com.serotonin.bacnet4j.type.primitive.Boolean;
+import com.serotonin.bacnet4j.type.primitive.Date;
 import com.serotonin.bacnet4j.type.primitive.Null;
 import com.serotonin.bacnet4j.type.primitive.Real;
+import com.serotonin.bacnet4j.type.primitive.SignedInteger;
+import com.serotonin.bacnet4j.type.primitive.Time;
+import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
+import java.util.GregorianCalendar;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 import org.code_house.bacnet4j.wrapper.api.BacNetClient;
 import org.code_house.bacnet4j.wrapper.api.BacNetToJavaConverter;
 import org.code_house.bacnet4j.wrapper.api.Property;
+import org.eclipse.smarthome.core.library.types.DateTimeType;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
@@ -56,7 +63,19 @@ public class ReadPropertyTask implements Runnable, BacNetToJavaConverter<State> 
       return new DecimalType(((Real) encodable).floatValue());
     } else if (encodable instanceof BinaryPV) {
       return encodable == BinaryPV.active ? OnOffType.ON : OnOffType.OFF;
+    } else if (encodable instanceof UnsignedInteger) {
+      return new DecimalType(((UnsignedInteger) encodable).intValue());
+    } else if (encodable instanceof SignedInteger) {
+      return new DecimalType(((SignedInteger) encodable).intValue());
+    } else if (encodable instanceof Boolean) {
+      return Boolean.TRUE == encodable ? OnOffType.ON : OnOffType.OFF;
+    } else if (encodable instanceof Time) {
+      Time time = (Time) encodable;
+      // HH:mm:ss.SSSZ
+      String millis = time.getHundredth() != 0 ? "." + time.getHundredth() : "";
+      return new DateTimeType(time.getHour() + ":" + time.getMinute() + ":" + time.getSecond() + millis);
     }
+    logger.info("Received value {}, {}", encodable, encodable.getClass().getName());
     return null;
   }
 }
