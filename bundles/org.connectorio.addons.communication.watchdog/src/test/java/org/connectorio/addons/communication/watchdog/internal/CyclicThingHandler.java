f2343e3cb1d2a5bf2628e32ad017c8e2774aa2f7
Łukasz Dywicki <luke@code-house.org>
2023-02-01 13:36:40 +0100

    Solve compile errors in watchdog.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- bundles/org.connectorio.addons.communication.watchdog/src/test/java/org/connectorio/addons/communication/watchdog/internal/CyclicThingHandler.java
+++ bundles/org.connectorio.addons.communication.watchdog/src/test/java/org/connectorio/addons/communication/watchdog/internal/CyclicThingHandler.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright (C) 2023-2023 ConnectorIO Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.connectorio.addons.communication.watchdog.internal;
 
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import org.connectorio.addons.communication.watchdog.Watchdog;
 import org.connectorio.addons.communication.watchdog.contrib.ThingStatusWatchdogListener;
@@ -7,18 +25,22 @@ import org.connectorio.addons.communication.watchdog.WatchdogManager;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
-import org.openhab.core.thing.binding.BaseThingHandler;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerCallback;
 import org.openhab.core.types.Command;
-import org.openhab.core.types.UnDefType;
 
-public class CyclicThingHandler extends BaseThingHandler {
+// This class does not use base class from openHAB lib due to "non-null" annotation processing
+// troubles. Beyond that it is ready for reuse.
+public abstract class CyclicThingHandler implements ThingHandler {
+
+  private final ScheduledExecutorService scheduler = null;
 
   private WatchdogManager watchdogManager;
   private Watchdog watchdog;
 
   // tag::injection[]
+  @SuppressWarnings("all")
   public CyclicThingHandler(Thing thing, WatchdogManager watchdogManager) {
-    super(thing);
     this.watchdogManager = watchdogManager;
   }
   // end::injection[]
@@ -39,6 +61,7 @@ public class CyclicThingHandler extends BaseThingHandler {
       watchdog.getCallbackWrapper().stateUpdated(channelUID, OnOffType.ON); // <5>
     }, 0, 1000, TimeUnit.MILLISECONDS);
   }
+
   // end::initialization[]
 
   // tag::close[]
@@ -54,4 +77,6 @@ public class CyclicThingHandler extends BaseThingHandler {
   public void handleCommand(ChannelUID channelUID, Command command) {
 
   }
+
+  protected abstract ThingHandlerCallback getCallback();
 }
--- bundles/org.connectorio.addons.communication.watchdog/src/test/java/org/connectorio/addons/communication/watchdog/internal/DefaultWatchdogManagerTest.java
+++ bundles/org.connectorio.addons.communication.watchdog/src/test/java/org/connectorio/addons/communication/watchdog/internal/DefaultWatchdogManagerTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023-2023 ConnectorIO Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.connectorio.addons.communication.watchdog.internal;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -6,8 +23,6 @@ import static org.mockito.Mockito.when;
 import org.connectorio.addons.communication.watchdog.Watchdog;
 import org.connectorio.addons.communication.watchdog.WatchdogBuilder;
 import org.connectorio.addons.communication.watchdog.WatchdogClock;
-import org.connectorio.addons.communication.watchdog.WatchdogEvent.WatchdogRecoveryEvent;
-import org.connectorio.addons.communication.watchdog.WatchdogEvent.WatchdogTimeoutEvent;
 import org.connectorio.addons.communication.watchdog.WatchdogListener;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
