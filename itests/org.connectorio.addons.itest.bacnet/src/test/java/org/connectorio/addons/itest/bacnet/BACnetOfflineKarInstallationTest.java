2018a558fac12dd54edbe11060aba52fcc78a48d
Łukasz Dywicki <luke@code-house.org>
2024-04-12 19:00:24 +0200

    Update bacnet component license declaration and headers.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- itests/org.connectorio.addons.itest.bacnet/src/test/java/org/connectorio/addons/itest/bacnet/BACnetOfflineKarInstallationTest.java
+++ itests/org.connectorio.addons.itest.bacnet/src/test/java/org/connectorio/addons/itest/bacnet/BACnetOfflineKarInstallationTest.java
@@ -1,19 +1,23 @@
 /*
  * Copyright (C) 2022-2022 ConnectorIO Sp. z o.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.gnu.org/licenses/gpl-3.0.txt
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * SPDX-License-Identifier: Apache-2.0
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 package org.connectorio.addons.itest.bacnet;
 
