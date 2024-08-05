a2dc43e3e4b77277cb3b4aee53864d423eab025a
Łukasz Dywicki <luke@code-house.org>
2024-08-05 15:25:47 +0200

    First push of more advanced version of OCPP binding.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

---
+++ bundles/org.connectorio.addons.binding.ocpp/src/main/java/org/connectorio/addons/binding/ocpp/internal/server/ConfigKeyConstants.java
@@ -0,0 +1,14 @@
+package org.connectorio.addons.binding.ocpp.internal.server;
+
+public interface ConfigKeyConstants {
+
+  // 4
+  String NUMBER_OF_CONNECTORS = "numberOfConnectors";
+  // Core,LocalAuthListManagement,RemoteTrigger,SoC
+  String SUPPORTED_FEATURE_PROFILES = "supportedFeatureProfiles";
+  // 60
+  String HEARTBEAT_INTERVAL = "heartbeatInterval";
+  // 60
+  String METER_VALUE_INTERVAL = "meterValueInterval";
+
+}
