4bcb85356203ba6e520f62437d668700a8720e95
Łukasz Dywicki <luke@code-house.org>
2026-04-10 00:49:48 +0400

    Update distribution URLs.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- bundles/org.connectorio.addons.binding.ocpp/src/main/java/org/connectorio/addons/binding/ocpp/internal/handler/ConnectorThingHandler.java
+++ bundles/org.connectorio.addons.binding.ocpp/src/main/java/org/connectorio/addons/binding/ocpp/internal/handler/ConnectorThingHandler.java
@@ -16,6 +16,7 @@ import eu.chargetime.ocpp.model.core.StopTransactionRequest;
 import eu.chargetime.ocpp.model.core.ValueFormat;
 import java.time.ZonedDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.measure.Quantity;
 import org.connectorio.addons.binding.handler.GenericThingHandlerBase;
 import org.connectorio.addons.binding.ocpp.internal.config.ChargerConfig;
 import org.connectorio.addons.binding.ocpp.internal.server.OcppMeasurementMapping;
@@ -36,7 +37,6 @@ import org.openhab.core.types.Command;
 import org.openhab.core.types.State;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tec.uom.se.ComparableQuantity;
 import tec.uom.se.quantity.Quantities;
 
 public class ConnectorThingHandler extends GenericThingHandlerBase<ServerBridgeHandler, ChargerConfig> implements
@@ -143,12 +143,12 @@ public class ConnectorThingHandler extends GenericThingHandlerBase<ServerBridgeH
   private static State parse(Double measurement, ChannelUID uid, SampledValue sample) {
     String unit = sample.getUnit();
     if (unit != null) {
-	  // Normalize unit names that don't match JSR-385 format
-	  switch (unit) {
-	    case "Celsius": unit = "°C"; break;
-	    case "Fahrenheit": unit = "°F"; break;
-	    default: break;
-	  }
+    // Normalize unit names that don't match JSR-385 format
+    switch (unit) {
+      case "Celsius": unit = "°C"; break;
+      case "Fahrenheit": unit = "°F"; break;
+      default: break;
+    }
 
       Quantity<?> quantity = Quantities.getQuantity("1 " + unit);
       return new QuantityType<>(measurement, quantity.getUnit());
