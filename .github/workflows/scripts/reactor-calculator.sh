8b7624c82c289d99919d95522c8738c583f8bc78
Łukasz Dywicki <luke@code-house.org>
2026-06-05 23:01:47 +0400

    Experimental support for partial PR builds.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

---
+++ .github/workflows/scripts/reactor-calculator.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+commits="${1:-HEAD~1}"
+default_options=""
+modules_to_build=""
+
+changes=$(git diff --name-only $commits | cut -d / -f 1-2)
+
+for path in $changes; do
+  if [[ ! "$path" =~ ^(bundles|features|itests|kars)/ ]]; then
+    echo "${default_options}"
+    exit 0
+  fi
+
+  module=$(cut -d'/' -f2 <<< "$path")
+
+  if [[ ! "$modules_to_build" =~ "$module" ]]; then
+    modules_to_build="${modules_to_build} -pl :$module"
+  fi
+done
+
+[ -n "$modules_to_build" ] && echo "${default_options} $modules_to_build -am -amd"
